3a71e5f61449f51e33f3bd63fdb91e50f67f226e
Poma_ob97@itstepru.onmicrosoft.com <Poma_ob97@itstepru.onmicrosoft.com>
2022-02-10 20:14:13 +0500

    update

--- WinApi/main.cpp
+++ WinApi/main.cpp
@@ -1,6 +1,9 @@
 #include<Windows.h>
 #include"resource.h"
 
+CONST CHAR INVITATION[] = "Введите логин";
+CONST CHAR INVITATION1[] = "Введите пароль";
+
 //#define MESSAGE_BOX
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -20,15 +23,69 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, in
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	//hwnd - (окно)
+	//uMsg - (сообщение)
+	//wParam - (параметры сообщения)
 	switch (uMsg)
 	{
-	case WM_INITDIALOG:break;
+	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, 0, (LPARAM)INVITATION);
+	}
+		break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
+		case IDC_BTN_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR buffer[SIZE]{};
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT1);
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)buffer);
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)buffer);
+		}
+		break;
 		case IDOK:MessageBox(NULL, "Была нажата кнопка ОК", "Info", MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
+		case IDC_EDIT1:
+		{
+			CONST INT SIZE = 256;
+			CHAR buffer[SIZE]{};
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_GETTEXT, SIZE, (LPARAM)buffer);
+			switch (HIWORD(wParam))
+			{
+			case EN_SETFOCUS:
+				
+				if (strcmp(buffer, INVITATION) == 0)
+					SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, 0, (LPARAM)"");
+				break;
+			case EN_KILLFOCUS:
+				//SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_GETTEXT, SIZE, (LPARAM)buffer);
+				if (strcmp(buffer, "") == 0)SendMessage(GetDlgItem(hwnd, IDC_EDIT1), WM_SETTEXT, 0, (LPARAM)INVITATION);
+			}
+			break;
+		}
+		case IDC_EDIT2:
+		{
+			CONST INT SIZE = 256;
+			CHAR buffer[SIZE]{};
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_GETTEXT, SIZE, (LPARAM)buffer);
+			switch (HIWORD(wParam))
+			{
+			case EN_SETFOCUS:
+
+				if (strcmp(buffer, INVITATION1) == 0)
+					SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)"");
+				break;
+			case EN_KILLFOCUS:
+				//SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_GETTEXT, SIZE, (LPARAM)buffer);
+				if (strcmp(buffer, "") == 0)SendMessage(GetDlgItem(hwnd, IDC_EDIT2), WM_SETTEXT, 0, (LPARAM)INVITATION1);
+			}
+			break;
+		}
 		}
 	}
 	break;
--- WinApi/resource.h
+++ WinApi/resource.h
@@ -2,15 +2,22 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
+#define IDOK2                           3
+#define IDCANCEL2                       4
 #define IDD_DIALOG1                     101
+#define IDI_ICON1                       103
+#define IDC_EDIT1                       1002
+#define IDC_EDIT2                       1003
+#define IDC_BTN_COPY2                   1004
+#define IDC_BTN_COPY                    1004
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
